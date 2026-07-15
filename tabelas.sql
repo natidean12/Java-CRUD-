e2817d23d9400f92e46f0c08fb38c66934abeeb5
Natasha <162047414+natidean12@users.noreply.github.com>
2026-07-15 03:32:29 -0300

    Create tabelas.sql

---
+++ tabelas.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE sistema_escolar;
+USE sistema_escolar;
+
+CREATE TABLE usuario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    login VARCHAR(50) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL -- O ideal é salvar a senha criptografada (ex: MD5, SHA-256 ou BCrypt)
+);
+
+-- Inserindo um usuário de teste (Senha: 123456)
+INSERT INTO usuario (nome, login, senha) 
+VALUES ('Administrador', 'admin', '123456');
+
